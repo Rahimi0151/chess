6baffc4b98818eb7983b1c8ab16654e808ba971d
rahimi0151 <rahimi0151@gmail.com>
2019-03-22 19:01:42 +0430

    queen can't jump now

--- src/Piece_Queen.py
+++ src/Piece_Queen.py
@@ -1,16 +1,59 @@
 from Piece import Piece
 
 class Piece_Queen ( Piece ):
-    def is_allowed_move ( self , toX , toY ):
-        fromX = self.get_position()[0]
-        fromY = self.get_position()[1]
 
-        if not( 
-                ( fromX - toX == 0 ) or ( fromY - toY == 0 ) or
-                ( abs( fromX - toX ) - abs( fromY - toY) ) == 0 
-            ):
+    def is_going_horizental( self , toX , toY ):
+        return ( self.getY() == toY )
+
+    def is_going_vertical( self , toX , toY ):
+        return ( self.getX() == toX )
+
+    def is_going_diagonal( self , toX , toY ):
+        fromX = self.getX()
+        fromY = self.getY()
+
+        return abs(abs(toX)-abs(fromX)) == abs(abs(toY)-abs(fromY)) 
+
+    def is_going_correctly( self , toX , toY ):
+        return ( self.is_going_horizental( toX , toY ) 
+        or self.is_going_vertical( toX , toY ) 
+        or self.is_going_diagonal( toX , toY ) )
+
+    def is_jumping( self , toX , toY ):
+        fromX = self.getX()
+        fromY = self.getY()
+
+        if ( self.is_going_diagonal( toX , toY ) ):
+            for x in range ( fromX , toX ):
+                for y in range ( fromY , toY ):
+                    if ( abs(abs(x)-abs(fromX)) == abs(abs(y)-abs(fromY)) 
+                    and not (x == fromX) 
+                    and not self.board.item[x][y] == None
+                    ):
+                        return True
+        
+        elif ( self.is_going_horizental( toX , toY ) ):
+            for x in range( fromX , toX ):
+                if x == fromX:
+                    continue
+                if not ( self.board.item[x][toY] == None and not(x==fromX)):
+                    return True
+
+        elif ( self.is_going_vertical( toX , toY ) ):
+            for y in range( fromY , toY ):
+                if y == fromY:
+                    continue
+                if not ( self.board.item[toX][y] == None ):
+                    return True
+
+    def is_allowed_move ( self , toX , toY ):
+
+        if not( self.is_going_correctly( toX , toY ) ):
             raise Exception("invalid move: queen can only move in a straght or diagonal line")
+        if self.is_jumping( toX , toY ):
+            raise Exception("invalid move: queen can't jump over any piece")
         return True
+
 
     def go_to( self , X , Y ):
         if self.is_allowed_move( X , Y ):
--- src/test_piece_queen.py
+++ src/test_piece_queen.py
@@ -10,11 +10,50 @@ def test_queen_can_go_in_diagonal_or_straight_line():
     queen.go_to( 3 , 4 )
     assert queen.get_position() == ( 3 , 4 )
 
-    # Straint
+    # Horizental
     queen.go_to( 7 , 4 )
     assert queen.get_position() == ( 7 , 4 )
 
+    # Vertical
+    queen.go_to( 7 , 1 )
+    assert queen.get_position() == ( 7 , 1 )
+
     # Not other types
     with pytest.raises(Exception):
-        queen.go_to( 3 , 5 )
-    assert queen.get_position() == ( 7 , 4 )
+        queen.go_to( 6 , 5 )
+    assert queen.get_position() == ( 7 , 1 )
+
+def test_queen_cant_jump_over_any_piece():
+
+    # diagonal
+    board = Board()
+    queen1 = Piece_Queen( board , "Black" , 1 , 1)
+    queen2 = Piece_Queen( board , "Black" , 2 , 2)
+
+    with pytest.raises(Exception):
+        queen1.go_to( 3 , 3 )
+    assert queen1.get_position() == ( 1 , 1 )
+
+
+    # vertical
+    board = Board()
+    queen1 = Piece_Queen( board , "Black" , 1 , 1)
+    queen2 = Piece_Queen( board , "Black" , 1 , 2)
+    
+    with pytest.raises(Exception):
+        queen1.go_to( 1 , 3 )
+    assert queen1.get_position() == ( 1 , 1 )
+
+
+    # horizental
+    board = Board()
+    queen1 = Piece_Queen( board , "Black" , 1 , 1)
+    queen2 = Piece_Queen( board , "Black" , 2 , 1)
+    
+    with pytest.raises(Exception):
+        queen1.go_to( 3 , 1 )
+    assert queen1.get_position() == ( 1 , 1 )
+
+
+
+
